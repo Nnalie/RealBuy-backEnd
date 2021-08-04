837d2e65e420341d39fdfa7155f6edbb44409924
Nnalie <leonardoelian.c.a@gmail.com>
2021-08-04 19:35:39 -0400

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/leonardoelian/ecommerceAPI/resources/CategoriaResource.java
+++ src/main/java/com/leonardoelian/ecommerceAPI/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.leonardoelian.ecommerceAPI.resources;
 
+import com.leonardoelian.ecommerceAPI.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar() {
-        return "Rest está funcionando!";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> listCat = new ArrayList<>();
+        listCat.add(cat1);
+        listCat.add(cat2);
+
+        return listCat;
     }
 
 }
