30f7a1375d71ed82b878a7fb344fdd0a8b7fd4e9
Nnalie <leonardoelian.c.a@gmail.com>
2021-08-27 11:04:40 -0400

    Estado e Cidade

--- src/main/java/com/leonardoelian/ecommerceAPI/EcommerceApiApplication.java
+++ src/main/java/com/leonardoelian/ecommerceAPI/EcommerceApiApplication.java
@@ -1,8 +1,12 @@
 package com.leonardoelian.ecommerceAPI;
 
 import com.leonardoelian.ecommerceAPI.domain.Categoria;
+import com.leonardoelian.ecommerceAPI.domain.Cidade;
+import com.leonardoelian.ecommerceAPI.domain.Estado;
 import com.leonardoelian.ecommerceAPI.domain.Produto;
 import com.leonardoelian.ecommerceAPI.repositories.CategoriaRepository;
+import com.leonardoelian.ecommerceAPI.repositories.CidadeRepository;
+import com.leonardoelian.ecommerceAPI.repositories.EstadoRepository;
 import com.leonardoelian.ecommerceAPI.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +22,12 @@ public class EcommerceApiApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApiApplication.class, args);
@@ -41,6 +51,19 @@ public class EcommerceApiApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est1.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 }
