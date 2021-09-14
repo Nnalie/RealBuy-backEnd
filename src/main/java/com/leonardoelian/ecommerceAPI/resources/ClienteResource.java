0e1e2a492005d0946058dbb507339f16942449b1
Leonardo Elian <leonardoelian.c.a@gmail.com>
2021-09-14 08:03:41 -0400

    PUT, DELETE e GET para Cliente

--- src/main/java/com/leonardoelian/ecommerceAPI/resources/ClienteResource.java
+++ src/main/java/com/leonardoelian/ecommerceAPI/resources/ClienteResource.java
@@ -1,14 +1,17 @@
 package com.leonardoelian.ecommerceAPI.resources;
 
 import com.leonardoelian.ecommerceAPI.domain.Cliente;
+import com.leonardoelian.ecommerceAPI.domain.Cliente;
+import com.leonardoelian.ecommerceAPI.dto.ClienteDTO;
 import com.leonardoelian.ecommerceAPI.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,22 +19,60 @@ import java.util.List;
 public class ClienteResource {
 
     @Autowired
-    private ClienteService cliServ;
+    private ClienteService service;
 
     @GetMapping
     public ResponseEntity<?> findAll() {
 
-        List<Cliente> obj = cliServ.findAll();
+        List<Cliente> obj = service.findAll();
 
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<?> findById(@PathVariable Integer id) {
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<ClienteDTO>> findPerPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 
-        Cliente obj = cliServ.findById(id);
+        Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
+
+        Cliente obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto) {
+        service.validateDTO(objDto);
+        Cliente obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody ClienteDTO objDto,
+                                       @PathVariable Integer id) {
+        service.validateDTO(objDto);
+        Cliente categoria = service.fromDTO(objDto);
+        categoria.setId(id);
+        categoria = service.update(categoria);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/leonardoelian/ecommerceAPI/services/ClienteService.java
+++ src/main/java/com/leonardoelian/ecommerceAPI/services/ClienteService.java
@@ -1,11 +1,15 @@
 package com.leonardoelian.ecommerceAPI.services;
 
-import com.leonardoelian.ecommerceAPI.domain.Categoria;
 import com.leonardoelian.ecommerceAPI.domain.Cliente;
-import com.leonardoelian.ecommerceAPI.repositories.CategoriaRepository;
+import com.leonardoelian.ecommerceAPI.dto.ClienteDTO;
 import com.leonardoelian.ecommerceAPI.repositories.ClienteRepository;
+import com.leonardoelian.ecommerceAPI.services.exceptions.DataIntegrityException;
 import com.leonardoelian.ecommerceAPI.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,17 +19,61 @@ import java.util.Optional;
 public class ClienteService {
 
     @Autowired
-    private ClienteRepository cliRepo;
+    private ClienteRepository repo;
 
     public List<Cliente> findAll() {
-        List<Cliente> obj = cliRepo.findAll();
+        List<Cliente> obj = repo.findAll();
         return obj;
     }
 
     public Cliente findById(Integer id) {
-        Optional<Cliente> obj = cliRepo.findById(id);
+        Optional<Cliente> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
-                "Categoria não encontrada! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+                "Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+    public Cliente insert(Cliente cliente) {
+        cliente.setId(null);
+        return repo.save(cliente);
+    }
+
+    public Cliente update(Cliente cliente) {
+        Cliente newCli = findById(cliente.getId());
+        updateData(newCli, cliente);
+        return repo.save(newCli);
+    }
+
+    private void updateData(Cliente newCli, Cliente cliente) {
+        newCli.setNome(cliente.getNome());
+        newCli.setEmail(cliente.getEmail());
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            repo.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma cliente que possui endereços associados a ele.");
+        }
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+    public void validateDTO(ClienteDTO clienteDTO) {
+        if(clienteDTO.getNome() == "" || clienteDTO.getEmail() == "") {
+            throw new DataIntegrityException("Preenchimento obrigatório dos campos");
+        }
+        else if(clienteDTO.getNome().length() < 5 && clienteDTO.getNome().length() > 120) {
+            throw new DataIntegrityException("O tamanho deve ser entre 5 e 80 caracteres");
+        }
     }
 
 }
