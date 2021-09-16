8abc786efad10c6d75352501b83732ce63d082f5
Leonardo Elian <leonardoelian.c.a@gmail.com>
2021-09-16 10:49:45 -0400

    Criando profile de desenvolvimento

--- src/main/java/com/leonardoelian/ecommerceAPI/config/DevConfig.java
+++ src/main/java/com/leonardoelian/ecommerceAPI/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.leonardoelian.ecommerceAPI.config;
 
 import com.leonardoelian.ecommerceAPI.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,15 +10,21 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
 
     @Autowired
     private DBService dbService;
 
     @Bean
     public boolean instantiateDataBase() throws ParseException {
+
+        if(!"create".equals(strategy)) {
+            return false;
+        }
         dbService.instantiateTestDataBase();
         return true;
     }
